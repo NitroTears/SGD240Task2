352ee5be58f0171141569da156e13ff3ef679129
Luke <lkellett2@outlook.com>
2021-08-03 08:39:16 +1000

    Added Player XP calculation algorithm

--- Assets/Scripts/XPHandler.cs
+++ Assets/Scripts/XPHandler.cs
@@ -17,13 +17,32 @@ public class XPHandler : MonoBehaviour
 {
     private void OnEnable()
     {
+        GameEvents.OnBattleConclude += GainXP;
     }
 
     private void OnDisable()
     {
+        GameEvents.OnBattleConclude -= GainXP;
     }
 
     public void GainXP(BattleResultEventData data)
     {
+        // If the player won the battle.
+        if (data.outcome > 0)
+        {
+            // XP calculation using level comparisons and win value in formula.
+            // Beating higher level NPCs will reward more XP.
+            var lvDiff = data.npc.level - data.player.level;
+            float firstCalculation = 1 + (lvDiff * 0.1f);
+            float firstMultiplier = firstCalculation <= 0.1f ? 0.1f : firstCalculation;
+            float secondCalculation = 1 + (0.25f * data.outcome);
+            float secondMultiplier = secondCalculation <= 0.1f ? 0.1f : secondCalculation;
+            var xp = Mathf.RoundToInt(firstMultiplier * secondMultiplier * 100);
+            if (xp >= 15) //Guarantees at least some XP will be awarded on winning.
+            {
+                xp = 15;
+            }
+            GameEvents.PlayerXPGain(xp);
+        }
     }
 }
